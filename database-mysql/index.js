5ee17e1d37dc6ea6f11cf4b47c58a6f1967cca4a
Kristie Desiree <kly5@mail.sfsu.edu>
2018-08-28 04:59:38 -0700

    Partial form

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -18,3 +18,41 @@ pgClient.connect((err) => {
   }
 });
 
+const postPets = () => {
+  const queryStr =
+    'INSERT INTO pet (name, gender, birthdate, species, breed, weight, microchipid) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+  const values = [];
+  db.query(queryStr, values, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      callback(data.rows);
+    }
+  });
+};
+
+const postVaccines = () => {
+  const queryStr =
+    'INSERT INTO vaccinations (petid, name, frequency, lastgiven) VALUES ($1, $2, $3, $4)';
+  const values = [];
+  db.query(queryStr, values, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      callback(data.rows);
+    }
+  });
+};
+
+const postMedications = () => {
+  const queryStr =
+    'INSERT INTO pet (petid, name, use, dosage, frequency) VALUES ($1, $2, $3, $4, $5)';
+  const values = [];
+  db.query(queryStr, values, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      callback(data.rows);
+    }
+  });
+};
